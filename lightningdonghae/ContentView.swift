e7301b79cbe9637eb9550ec05d12197f34697038
tenedict <tenedict@gmail.com>
2024-09-24 18:11:48 +0900

    first commit

---
+++ lightningdonghae/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lightningdonghae
+//
+//  Created by 문재윤 on 9/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lightningdonghae/lightningdonghaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  lightningdonghaeApp.swift
+//  lightningdonghae
+//
+//  Created by 문재윤 on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct lightningdonghaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
